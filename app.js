c4823770ce29af9b9665ca50bd191b1a417970c2
raulerik <raul.erik.laur@gmail.com>
2022-11-04 20:16:38 +0200

    Add book to localStorage

--- app.js
+++ app.js
@@ -1,32 +1,39 @@
-
 const form = document.querySelector('#book-form');
 const booksList = document.querySelector('#book-list');
-
+const deleteBooks = document.querySelector('#delete-books')
 
 // events
 form.addEventListener('submit', addBook);
 booksList.addEventListener('click', deleteBook);
-
-//get form input data
-const titleInput = document.querySelector('#title');
-const authorInput = document.querySelector('#author');
-const isbnInput = document.querySelector('#isbn');
+deleteBooks.addEventListener('click', deleteAllBooks);
 
 
 function addBook(e) {
+    //get form input data
+    const titleInput = document.querySelector('#title');
+    const authorInput = document.querySelector('#author');
+    const isbnInput = document.querySelector('#isbn');
     // input value
     const title = titleInput.value;
     const author = authorInput.value;
     const isbn = isbnInput.value;
     // create <tr> element
     const row = document.createElement('tr');
+    // create book
+    const book = [title, author, isbn];
 
-    row.innerHTML = `
-      <td>${title}</td>
-      <td>${author}</td>
-      <td>${isbn}</td>
-      <td><a href="#">X</a></td>
-    `;
+    book.forEach((dataItem) => {
+        const td = document.createElement('td');
+        td.appendChild(document.createTextNode(dataItem));
+        row.appendChild(td);
+    })
+
+    const td = document.createElement('td');
+    const link = document.createElement('a');
+    link.setAttribute('href', '#');
+    link.appendChild(document.createTextNode('X'));
+    td.appendChild(link);
+    row.appendChild(td);
 
     booksList.appendChild(row);
     // clear input value
@@ -34,6 +41,8 @@ function addBook(e) {
     authorInput.value = '';
     isbnInput.value = '';
 
+    // add book to local storage
+    addBookToLS(book);
     // form submit event control
     e.preventDefault();
     }
@@ -43,7 +52,28 @@ function addBook(e) {
 function deleteBook(e) {
     if(e.target.textContent === 'X') {
         if(confirm('Are you sure to delete this book?')) {
-            e.target.parentElement.remove()
+            e.target.parentElement.parentElement.remove()
             }
     }
+}
+
+// delete all books
+function deleteAllBooks(e) {
+    if(confirm('Are you sure to delete all books?')) {
+    while (booksList.firstChild){
+        booksList.removeChild(booksList.firstChild)
+    }
+    }
+}
+
+// add book to local storage
+function addBookToLS(book) {
+    let books
+    if(localStorage.getItem('books') === null) {
+        books = []
+    } else {
+        books = JSON.parse(localStorage.getItem('books'))
+    }
+    books.push(book)
+    localStorage.setItem('books', JSON.stringify(books))
 }
